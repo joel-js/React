221b926a1774d46e82cd26825cac4d926fe6e7e0
joel-js <joel.js@inapp.com>
2022-09-19 15:32:46 +0530

    hooks start

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,19 +1,22 @@
-import './App.css';
+
 import Navbar from './Navbar';
 import Home from './Home';
 function App() {
-  const txt   = "welcome";
-  const likes = 50
+  
+	const txt   = "welcome";
+  	const likes = 50
   // const likes = {name:"jj"} 
   // const likes = true
   // the above lines cause error, as objects and boolean cannot be printed but can be accessed
-  return (
-    <div className='App'>
-      <Navbar></Navbar>
-      <div className="content">
-        <Home></Home>
+	
 
-      </div>
+  return (
+   	<div className='App'>
+   		<Navbar></Navbar>
+   		<div className="content">
+      			<Home></Home>
+					
+      	</div>
     </div>
   );
 }
--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,6 +1,19 @@
+import { useState } from "react"
+
 const Home = () => {
+    
+    
+    const [num, setNum] = useState(5)
+    const handleClick = (event) => {
+        setNum(parseInt(Math.random()*1000))
+		console.log("button clicked "+num+" target: ",event.target);
+	}
+
     return (
-        <h2>Hoola</h2>
+        <div className="home">
+            <h2>{num}</h2>
+            <button onClick={ (event) => handleClick(event) }> button </button>
+        </div>
     );
 }
  
