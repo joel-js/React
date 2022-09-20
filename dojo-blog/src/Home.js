3ad344dc9adc69f7bedd930ac326be62e2683cf0
joel-js <joel.js@inapp.com>
2022-09-20 10:07:16 +0530

    testing states

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -2,8 +2,17 @@ import { useState } from "react"
 
 const Home = () => {
     
-    
-    const [num, setNum] = useState(5)
+    const START = 5;
+    let val =START
+    let num1 = START, num2 = START;
+    console.log(val)
+    const [num, setNum] = useState(START)
+
+    if( num !== val){
+        val = num;
+        num1 = parseInt(Math.random()*1000)
+        num2 = parseInt(Math.random()*1000)
+    }
     const handleClick = (event) => {
         setNum(parseInt(Math.random()*1000))
 		console.log("button clicked "+num+" target: ",event.target);
@@ -11,7 +20,9 @@ const Home = () => {
 
     return (
         <div className="home">
-            <h2>{num}</h2>
+            <h2>{num1}</h2>
+            <h2>{num2}</h2>
+
             <button onClick={ (event) => handleClick(event) }> button </button>
         </div>
     );
