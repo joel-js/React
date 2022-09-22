b3dbf283bbfea8d023c5b35ba98f8b0349b7e113
joel-js <joel.js@inapp.com>
2022-09-22 12:04:40 +0530

    useEffect and useState okay

--- practice/t_001/src/Home.js
+++ practice/t_001/src/Home.js
@@ -1,10 +1,32 @@
+import { useEffect, useState } from 'react';
+import Button from './Button';
 import Card from './Card'
+
 const Home = () => {
+
+    let [count, setCount] = useState(0);
+    let [display,setDisplay] =useState(count)
+
+    let addCount = () =>{
+        setCount(count+=1)
+    }
+    
+    useEffect( () => setDisplay(Array(count).fill('*').map( () => <Card />)) ,[count])
     return (
+        
         <div className="home">
-            <div className="col s6">
-                <Card></Card>
+
+            <div className="row">
+                <div className="col s1">
+                    <Button addCount={addCount} count ={count} setCount={setCount}/>
+                </div>
             </div>
+
+            <div className="row">
+                <br />
+                {display}
+            </div> 
+
         </div>
     );
 }
