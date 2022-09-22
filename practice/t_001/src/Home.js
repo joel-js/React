f0856ed498e125958b46e4c791bad4e1ff13288b
joel-js <joel.js@inapp.com>
2022-09-22 12:34:20 +0530

    key work aaki

--- practice/t_001/src/Home.js
+++ practice/t_001/src/Home.js
@@ -1,34 +1,40 @@
-import { useEffect, useState } from 'react';
-import Button from './Button';
-import Card from './Card'
+import { useEffect, useState } from "react";
+import Button from "./Button";
+import Card from "./Card";
 
 const Home = () => {
+  let [count, setCount] = useState(0);
+  let [display, setDisplay] = useState(count);
+  let [countArray, setCountArray] = useState([]);
 
-    let [count, setCount] = useState(0);
-    let [display,setDisplay] =useState(count)
+  let addCount = () => {
+    setCount((count += 1));
+    setCountArray((prevCount)=> [...prevCount, count])
+  };
 
-    let addCount = () =>{
-        setCount(count+=1)
-    }
-    
-    useEffect( () => setDisplay(Array(count).fill('*').map( () => <Card />)) ,[count])
-    return (
-        
-        <div className="home">
-
-            <div className="row">
-                <div className="col s1">
-                    <Button addCount={addCount} count ={count} setCount={setCount}/>
-                </div>
-            </div>
-
-            <div className="row">
-                <br />
-                {display}
-            </div> 
-
-        </div>
+  useEffect(() => {
+    setDisplay(
+        countArray
+        .map((key, index) => <Card key={key} />)
     );
-}
- 
+  }, [count]);
+  return (
+    <div className="home">
+      <div className="row">
+        <div className="col s1">
+          <Button addCount={addCount} count={count} />
+        </div>
+      </div>
+
+      <div className="row">
+        <br />
+        <div className="col s4">
+          {count}
+          {display}
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Home;
