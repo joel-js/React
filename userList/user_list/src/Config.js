ae5635d89f2523c80790fdab0cb940cc21e7e6f9
joel-js <joel.js@inapp.com>
2022-10-11 11:06:51 +0530

    works now with obj.assign

--- userList/user_list/src/Config.js
+++ userList/user_list/src/Config.js
@@ -1,25 +1,17 @@
 import Axios from 'axios'
 
-export async function axiosTest(url) {
+export const axiosTest = async url => {
   return await Axios.get(url).then( res => res.data)
 }
 
-// export async function axiosGetPostComments(url,id){
-//   return await Axios.get(url+'/'+id+'/'+'comments').then( res => res.data)
-// }
-
-export async function axiosPostsPreview(url) {
-
-  return await axiosTest(url).then( async data => {
-    
-    const urls = data.map( item => Axios.get(url+'/'+item.id+'/comments') )
-    const res = await Promise.all(urls)
-    const return_data = res.map( res => res.data)
-    // data.map( post => {
-    //   return Axios.get(url+'/'+post.id+'/comments').then( res => res.data)
-    // })
-
-
-    return return_data;
+export const axiosPostsPreview = async url => {
+  return await axiosTest(url).then(  posts => {
+    posts.map( async post => {
+      // const res = await Axios.get(url+'/'+post.id+'/comments').then( res => res.data)
+      // post.comments = res
+      Object.assign(post,{commnets: await Axios.get(url+'/'+post.id+'/comments').then( res => res.data)})
+      
+    })
+    return posts;
   })
 }
