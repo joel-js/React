b0960754c02bf7927e3c4d44d9a00a466c669c39
joel-js <joel.js@inapp.com>
2022-11-01 14:52:52 +0530

    trying usecontext

---
+++ useContext/t_001/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <input type="text" />
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ useContext/t_001/src/C1.js
@@ -0,0 +1,9 @@
+const C1 = () => {
+  return (
+    <div className="C1">
+      
+    </div>
+  );
+}
+ 
+export default C1;
