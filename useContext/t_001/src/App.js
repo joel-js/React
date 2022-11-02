a6ac7494a3e0083ceb4eacd470b1c206bda08398
joel-js <joel.js@inapp.com>
2022-11-02 11:19:36 +0530

    saga to middleware and some other changes

--- useContext/t_001/src/App.js
+++ useContext/t_001/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <input type="text" />
+     
      
     </div>
   );
--- useContext/t_001/src/C1.js
+++
@@ -1,9 +0,0 @@
-const C1 = () => {
-  return (
-    <div className="C1">
-      
-    </div>
-  );
-}
- 
-export default C1;
